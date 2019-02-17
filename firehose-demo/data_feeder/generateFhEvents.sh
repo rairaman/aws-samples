3239872a6047c04c869cd0146622473529df3651
Raihaan Raman <raihaan@gmail.com>
2019-02-17 12:51:42 +1100

    Added usage to the data feeding script

--- firehose-demo/data_feeder/generateFhEvents.sh
+++ firehose-demo/data_feeder/generateFhEvents.sh
@@ -6,6 +6,11 @@ function usage()
     echo -e "Usage: $0 <firehose_delivery stream name>"
 }
 
+if [ "$#" -ne 1 ]; then
+    usage
+    exit 1
+fi
+
 DS_NAME=$1
 
 payload='{"Data":"{\"First_Name\":\"Quick Brown Fox\",\"Last_Name\":\"Name Me\"}"}'
